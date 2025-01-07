d970a65904652902c4e342260f7b006ebba3794c
Mateo Mikael Pillajo Guijarro <mateo.pillajo@udla.edu.ec>
2025-01-07 18:13:36 -0500

    Agregar archivos de proyecto.

---
+++ People/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace People;
+
+public partial class App : Application
+{
+	public static PersonRepository PersonRepo { get; private set; }
+
+	public App()
+	{
+		InitializeComponent();
+		// TODO: Initialize the PersonRepository property with the PersonRespository singleton object
+
+	}
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
